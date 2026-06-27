fbb80c07b45e448b7bc7b5a66a3a865a7301721c
krb511 <l2rd1000511@gmail.com>
2026-06-27 08:44:24 +0300

    Update TicketManager.ts

--- src/managers/TicketManager.ts
+++ src/managers/TicketManager.ts
@@ -1,11 +1,10 @@
-import { db } from '../database/prisma';
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
 
 export class TicketManager {
     public static async create(guildId: string, userId: string, category: string) {
         return await db.ticket.create({
-            data: {
-                guildId, userId, category, status: 'OPEN'
-            }
+            data: { guildId, userId, category, status: 'OPEN' }
         });
     }
 }
