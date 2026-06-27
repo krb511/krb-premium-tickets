c4d8b5dd0ab2e30a86d059d87ceec2ac56f359bd
krb511 <l2rd1000511@gmail.com>
2026-06-27 08:20:54 +0300

    Update TranscriptService.ts

--- src/services/TranscriptService.ts
+++ src/services/TranscriptService.ts
@@ -3,13 +3,12 @@ import * as discordTranscripts from 'discord-html-transcripts';
 
 export class TranscriptService {
     public static async generate(channel: TextChannel): Promise<AttachmentBuilder> {
-        // نطلب من المكتبة أن ترجع Attachment مباشرة
         const attachment = await discordTranscripts.createTranscript(channel, {
             returnType: 'attachment',
             filename: 'transcript.html',
         });
         
-        // التحقق أن النتيجة AttachmentBuilder
-        return attachment as AttachmentBuilder;
+        // التحويل الصريح لضمان توافق الأنواع
+        return attachment as unknown as AttachmentBuilder;
     }
 }
